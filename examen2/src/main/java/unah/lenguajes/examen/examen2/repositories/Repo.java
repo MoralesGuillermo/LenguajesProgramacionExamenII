85fbd512cd6907f0ef0216899efab14166980a72
Guillermo Morales <morales.guillermo2503@gmail.com>
2024-04-05 16:57:45 -0600

    word

--- examen2/src/main/java/unah/lenguajes/examen/examen2/repositories/Repo.java
+++ examen2/src/main/java/unah/lenguajes/examen/examen2/repositories/Repo.java
@@ -3,7 +3,6 @@ package unah.lenguajes.examen.examen2.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import ch.qos.logback.core.model.Model;
 
 @Repository
 public interface Repo extends CrudRepository<> {
